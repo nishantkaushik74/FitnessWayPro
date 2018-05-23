a8cece4fc96a8bdd6eaae05ce7cdbf82dc9d6733
nishant kaushik <kaushik.nishant74@gmail.com>
2018-05-23 22:03:15 +0530

    design changes

--- src/app/layout/ManageClient/manageclient.module.ts
+++ src/app/layout/ManageClient/manageclient.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { manageclientComponent } from './manageclient.component'
 import { manageclientRoutingModule } from './manageclient-routing.module'
 import { FormsModule } from '@angular/forms';
+import { PageHeaderModule } from '../../shared';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, manageclientRoutingModule],
+    imports: [CommonModule, FormsModule, manageclientRoutingModule, PageHeaderModule],
     declarations: [manageclientComponent]
 })
 export class manageclientModule { }
--- src/app/layout/managepackage/managepackage.module.ts
+++ src/app/layout/managepackage/managepackage.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { managepackageComponent } from './managepackage.component'
 import { managepackageRoutingModule } from './managepackage-routing.module'
 import { FormsModule } from '@angular/forms';
+import { PageHeaderModule } from '../../shared';
 
 @NgModule({
-    imports: [CommonModule, FormsModule, managepackageRoutingModule],
+    imports: [CommonModule, FormsModule, managepackageRoutingModule, PageHeaderModule],
     declarations: [managepackageComponent]
 })
 export class managepackageModule { }
